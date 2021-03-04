0fe764b4201b95436806eb41ba308af7f5235f72
Ilaay <52674359+Ilaay@users.noreply.github.com>
2021-03-04 11:34:30 +0200

    RGBColorMixer

---
+++ ColorMixer/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Drawing;
+using Console = Colorful.Console;
+
+namespace ColorMixer
+{
+    class Program
+    {
+        static void NewColorMixing()
+        {
+            Console.WriteLine("  First Color: ");
+            System.Console.Write("R: ");
+            byte x = Convert.ToByte(Console.ReadLine());
+            System.Console.Write("G: ");
+            byte y = Convert.ToByte(Console.ReadLine());
+            System.Console.Write("B: ");
+            byte z = Convert.ToByte(Console.ReadLine());
+            myColor firstColor = new myColor(x, y, z);
+
+
+            Console.WriteLine("  Second Color: ");
+            System.Console.Write("R: ");
+            byte x2 = Convert.ToByte(Console.ReadLine());
+            System.Console.Write("G: ");
+            byte y2 = Convert.ToByte(Console.ReadLine());
+            System.Console.Write("B: ");
+            byte z2 = Convert.ToByte(Console.ReadLine());
+            myColor secondColor = new myColor(x2, y2, z2);
+            firstColor.ShowUp();
+            System.Console.Write(" + ");
+            secondColor.ShowUp();
+            System.Console.Write(" = ");
+
+
+            myColor resultColor = new myColor();
+            myColor.MixResult(resultColor, firstColor, secondColor);
+            System.Console.WriteLine("\n");
+        }
+        static void Main(string[] args) //rgb color mixer
+        {
+            Console.ForegroundColor = Color.White;
+            Console.WriteLine("Programm to mix the RGB colors \n");
+            while (true)
+            {
+                NewColorMixing();
+            }
+        }
+    }
+}
---
+++ ColorMixer/myColor.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Drawing;
+using Console = Colorful.Console;
+
+namespace ColorMixer
+{
+    class myColor
+    {
+        byte R;
+        byte G;
+        byte B;
+
+        public myColor(byte r, byte g, byte b)
+        {
+           R = r;
+           G = g;
+           B = b;
+        }
+        public myColor()
+        {
+            R = 0;
+            G = 0;
+            B = 0;
+        }
+
+        public void ShowUp()
+        {
+            Console.Write("This Color", Color.FromArgb(R, G, B));
+        }
+
+        public static void MixResult(myColor resultColor, myColor firstColor, myColor secondColor)
+        {
+            resultColor.R = Convert.ToByte((firstColor.R + secondColor.R) / 2);
+            resultColor.G = Convert.ToByte((firstColor.G + secondColor.G) / 2);
+            resultColor.B = Convert.ToByte((firstColor.B + secondColor.B) / 2);
+            resultColor.ShowUp();
+        }
+
+    }
+}
